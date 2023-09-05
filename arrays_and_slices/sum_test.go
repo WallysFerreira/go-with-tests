38f32a614e4a61b5a49cf3d298e0602fe3331d93
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-05 20:40:08 -0300

    create SumAll test

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -12,3 +12,12 @@ func TestSum(t *testing.T) {
 		t.Errorf("Got %d expected %d, %v", got, expected, numbers)
 	}
 }
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if got != expected {
+		t.Errorf("Got %v expected %v", got, expected)
+	}
+}
