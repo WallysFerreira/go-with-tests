5de026b07c336879dfa34cfca74896dab8960726
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-05 17:36:35 +0000

    add French funcionality

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("world")
+		got := Hello("world", "")
 		want := "Hello, world"
 
 		assertCorrectMessages(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessages(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Teste", "Spanish")
+		want := "Hola, Teste"
+
+		assertCorrectMessages(t, got, want)
+	})
+
+	t.Run("in French", func (t *testing.T) {
+		got := Hello("Teste", "French")
+		want := "Bonjour, Teste"
 
 		assertCorrectMessages(t, got, want)
 	})
