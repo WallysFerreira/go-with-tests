591d0e3485b46113fbc0b528f926aabec365f1b1
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-05 20:34:09 -0300

    use only one test

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -3,14 +3,12 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collections of any sizes", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	numbers := []int{1, 2, 3}
 
-		got := Sum(numbers)
-		expected := 6
+	got := Sum(numbers)
+	expected := 6
 
-		if got != expected {
-			t.Errorf("Got %d expected %d, %v", got, expected, numbers)
-		}
-	})
+	if got != expected {
+		t.Errorf("Got %d expected %d, %v", got, expected, numbers)
+	}
 }
