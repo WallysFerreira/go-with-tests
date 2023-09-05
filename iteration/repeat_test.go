b244e5a547e9ddd3aef6b92470b89096da24113a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-05 18:31:04 +0000

    add parameter to choose how many repeats

---
+++ iteration/repeat_test.go
@@ -0,0 +1,18 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a", 7)
+	expected := "aaaaaaa"
+
+	if got != expected {
+		t.Errorf("Got %q expected %q", got, expected)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
+	}
+}
