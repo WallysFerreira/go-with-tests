034dceabb875d7414d8f9ba7aed6111aa5fce86a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-05 18:16:28 +0000

    add example code

---
+++ integers/adder_test.go
@@ -0,0 +1,25 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 2)
+	expected := 4
+
+	if got != expected {
+		t.Errorf("Got %d expected %d", got, expected)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
