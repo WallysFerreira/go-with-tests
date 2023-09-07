5b311353f0037784282d3f143d819f049317a83e
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 14:04:26 -0300

    refactor tests for Perimeter

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -2,17 +2,26 @@ package main
 
 import "testing"
 
-/*
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	expected := 40.0
+	perimeterTests := []struct {
+		name     string
+		shape    ShapeWithPerimeter
+		expected float64
+	}{
+		{name: "Rectangle", shape: Rectangle{10, 10}, expected: 40.0},
+		{name: "Circle", shape: Circle{10}, expected: 62.8318530718},
+	}
 
-	if got != expected {
-		t.Errorf("Got %.2f expected %.2f", got, expected)
+	for _, tt := range perimeterTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+
+			if got != tt.expected {
+				t.Errorf("%#v got %g expected %g", tt.shape, got, tt.expected)
+			}
+		})
 	}
 }
-*/
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
