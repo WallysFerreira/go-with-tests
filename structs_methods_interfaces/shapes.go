e91a1585dc4afc2439441caef322a6647fe7d511
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 13:22:59 -0300

    refactor tests and functions

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,11 +1,11 @@
 package main
 
-func Perimeter(height, width float64) float64 {
-	return 2 * (height + width)
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Height + r.Width)
 }
 
-func Area(height, width float64) float64 {
-	return height * width
+func Area(r Rectangle) float64 {
+	return r.Height * r.Width
 }
 
 type Rectangle struct {
