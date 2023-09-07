7a2313771cee518ddcc2bf6c07ffdd78da33b964
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 13:33:43 -0300

    refactor code to use methods

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -2,14 +2,6 @@ package main
 
 import "math"
 
-func Perimeter(r Rectangle) float64 {
-	return 2 * (r.Height + r.Width)
-}
-
-func Area(r Rectangle) float64 {
-	return r.Height * r.Width
-}
-
 type Rectangle struct {
 	Width  float64
 	Height float64
