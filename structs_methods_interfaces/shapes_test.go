527c93d1dd72b3e59dbff8fe3d1da59b70794708
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 13:26:55 -0300

    write tests for circle

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	expected := 100.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Area(rectangle)
+		expected := 100.0
 
-	if got != expected {
-		t.Errorf("Got %.2f expected %.2f", got, expected)
-	}
+		if got != expected {
+			t.Errorf("Got %.2f expected %.2f", got, expected)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		expected := 314.1592653589793
+
+		if got != expected {
+			t.Errorf("Got %.2f expected %.2f", got, expected)
+		}
+	})
 }
