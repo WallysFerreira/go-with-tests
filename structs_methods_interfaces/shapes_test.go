4f56e217491f7c1f3f82336619ef82ea060d0907
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 13:38:32 -0300

    refactor tests to use interface Shape

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -13,23 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		expected := 100.0
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+
+		got := shape.Area()
 
 		if got != expected {
 			t.Errorf("Got %.2f expected %.2f", got, expected)
 		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+
+		checkArea(t, rectangle, 100.0)
 	})
 
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		expected := 314.1592653589793
 
-		if got != expected {
-			t.Errorf("Got %.2f expected %.2f", got, expected)
-		}
+		checkArea(t, circle, expected)
 	})
 }
