1186ed660d0d39d09fb9e636e83c1727ed5c2d8c
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 21:32:29 -0300

    refactor code to use bitcoin

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -5,10 +5,10 @@ import "testing"
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	expected := 10
+	expected := Bitcoin(10)
 
 	if got != expected {
 		t.Errorf("%#v got %d expected %d", wallet, got, expected)
