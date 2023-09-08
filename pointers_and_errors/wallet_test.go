9e0b614b1084569f141df4d2042b8fe174c957c3
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-07 21:38:47 -0300

    create withdraw test

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -3,14 +3,28 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		expected := Bitcoin(10)
 
-	got := wallet.Balance()
-	expected := Bitcoin(10)
+		if got != expected {
+			t.Errorf("%#v got %s expected %s", wallet, got, expected)
+		}
+	})
 
-	if got != expected {
-		t.Errorf("%#v got %s expected %s", wallet, got, expected)
-	}
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		expected := Bitcoin(10)
+
+		if got != expected {
+			t.Errorf("%#v got %s expected %s", wallet, got, expected)
+		}
+	})
+
 }
