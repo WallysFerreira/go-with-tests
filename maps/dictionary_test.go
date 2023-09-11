368ecd19e291e5de1665e0178084ea24236a3774
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-10 21:25:40 -0300

    write tests

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	expected := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	assertStrings(t, got, expected)
+		assertStrings(t, got, expected)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("a")
+		expected := "Could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("Expected an error but didn't get one")
+		}
+
+		assertStrings(t, err.Error(), expected)
+	})
 }
 
 func assertStrings(t testing.TB, got, expected string) {
