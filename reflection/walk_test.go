e8cb5c2c14c310d37e48c2c795e20670819d419a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-11 15:24:44 -0300

    write test for channels

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -105,6 +105,27 @@ func TestWalk(t *testing.T) {
 		assertContains(t, got, "Moo")
 		assertContains(t, got, "Baa")
 	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		expected := []string{"Berlin", "Katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("got %v expected %v", got, expected)
+		}
+	})
 }
 
 func assertContains(t testing.TB, got []string, expected string) {
