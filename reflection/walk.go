93458e9cf3731ab9ca846d5dc8071ccab0a46a72
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-11 15:26:56 -0300

    deal with channels

--- reflection/walk.go
+++ reflection/walk.go
@@ -22,6 +22,10 @@ func walk(x interface{}, fn func(input string)) {
 		for _, key := range val.MapKeys() {
 			walkValue(val.MapIndex(key))
 		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
 	case reflect.String:
 		fn(val.String())
 	}
