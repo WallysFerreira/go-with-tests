6ffe678fa3e0dbd981b78c425d7d5cc2cb1bbdbf
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-11 09:44:02 -0300

    finish mocking chapter

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,21 +2,80 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
+	t.Run("prints 3 to 1 then Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	Countdown(buffer)
-
-	got := buffer.String()
-	expected := `3
+		got := buffer.String()
+		expected := `3
 2
 1
 Go!`
 
-	if got != expected {
-		t.Errorf("got %q expected %q", got, expected)
+		if got != expected {
+			t.Errorf("got %q expected %q", got, expected)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("expected calls %v, got %v", expected, spySleepPrinter.Calls)
+		}
+	})
+
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
--- mocking/main.go
+++ mocking/main.go
@@ -4,12 +4,35 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
