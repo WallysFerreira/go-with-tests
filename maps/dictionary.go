1c22257756fd60737139a1e500510361ca6ef398
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-11 08:27:47 -0300

    write method Update

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,8 +3,9 @@ package main
 type Dictionary map[string]string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -38,6 +39,17 @@ func (d Dictionary) Add(word, value string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, value string) {
-	d[word] = value
+func (d Dictionary) Update(word, value string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = value
+	default:
+		return err
+	}
+
+	return nil
 }
