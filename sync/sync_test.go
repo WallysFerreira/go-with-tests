0e7cf305de3372754d6f39809257a4214a7f7a16
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-25 17:54:07 -0300

    fix test

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -6,11 +6,12 @@ func TestCounter(t *testing.T) {
 	t.Run("imcrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 		counter := Counter{}
 		expected := 3
-		got := counter.Value()
 
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
+
+		got := counter.Value()
 
 		if got != expected {
 			t.Errorf("Got %d, expected %d", got, expected)
